6d8b2b505e23fa09bdd57f2e970e36d7794b8223
Nirajan Karki <nirajankarki1985@gmail.com>
2023-04-06 23:02:47 +0200

    Making methods aync

--- AppointmentSchedulerAPI/Controllers/AppointmentController.cs
+++ AppointmentSchedulerAPI/Controllers/AppointmentController.cs
@@ -19,22 +19,22 @@ namespace AppointmentSchedulerAPI.Controllers
 
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public ActionResult<IEnumerable<AppointmentDTO>> GetAppointments()
+        public async Task<ActionResult<IEnumerable<AppointmentDTO>>> GetAppointments()
         {
-            return Ok(_db.Appointments.ToList());
+            return Ok(await _db.Appointments.ToListAsync());
         }
 
         [HttpGet("{id:int}", Name = "GetAppointment")]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public ActionResult<AppointmentDTO> GetAppointment(int id)
+        public async Task< ActionResult<AppointmentDTO>> GetAppointment(int id)
         {
             if (id == 0)
             {
                 return BadRequest();
             }
-            var appointment = _db.Appointments.FirstOrDefault(u => u.Id == id);
+            var appointment =await _db.Appointments.FirstOrDefaultAsync(u => u.Id == id);
 
             if (appointment == null)
             {
@@ -46,9 +46,9 @@ namespace AppointmentSchedulerAPI.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [ProducesResponseType(StatusCodes.Status201Created)]
-        public ActionResult<AppointmentDTO> CreateAppointment([FromBody] AppointmentCreateDTO appointmentDTO)
+        public async Task< ActionResult<AppointmentDTO>> CreateAppointment([FromBody] AppointmentCreateDTO appointmentDTO)
         {
-            if (_db.Appointments.FirstOrDefault(u => u.Title.ToLower() == appointmentDTO.Title.ToLower()) != null)
+            if (await _db.Appointments.FirstOrDefaultAsync(u => u.Title.ToLower() == appointmentDTO.Title.ToLower()) != null)
             {
                 ModelState.AddModelError("CustomError", "Appointment already exists !");
                 return BadRequest(ModelState);
@@ -70,8 +70,8 @@ namespace AppointmentSchedulerAPI.Controllers
 
             };
 
-            _db.Appointments.Add(model);
-            _db.SaveChanges();
+            await _db.Appointments.AddAsync(model);
+            await _db.SaveChangesAsync();
 
             //return Ok(appointmentDTO);
             return CreatedAtRoute("GetAppointment", new { id = model.Id }, model);
@@ -80,27 +80,27 @@ namespace AppointmentSchedulerAPI.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public IActionResult DeleteAppointment(int id)
+        public async Task<IActionResult> DeleteAppointment(int id)
         {
             if (id == 0)
             {
                 return BadRequest();
             }
-            var appointment = _db.Appointments.FirstOrDefault(u => u.Id == id);
+            var appointment = await _db.Appointments.FirstOrDefaultAsync(u => u.Id == id);
             if (appointment == null)
             {
                 return NotFound();
             }
 
             _db.Appointments.Remove(appointment);
-            _db.SaveChanges();
+            await _db.SaveChangesAsync();
             return NoContent();
         }
         [HttpPut("{id:int}", Name = "UpdateAppointment")]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
 
-        public IActionResult UpdateAppointment(int id,[FromBody]AppointmentUpdateDTO appointmentDTO)
+        public async Task<IActionResult> UpdateAppointment(int id,[FromBody]AppointmentUpdateDTO appointmentDTO)
         {
             if(appointmentDTO==null || id!=appointmentDTO.Id)
             {
@@ -116,7 +116,7 @@ namespace AppointmentSchedulerAPI.Controllers
                 Reminder= appointmentDTO.Reminder,
             };
             _db.Appointments.Update(model);
-            _db.SaveChanges();
+            await _db.SaveChangesAsync();
             
             return NoContent();
         }
@@ -125,13 +125,13 @@ namespace AppointmentSchedulerAPI.Controllers
         [ProducesResponseType(StatusCodes.Status204NoContent)]
 
 
-        public IActionResult UpdatePartialAppointment(int id,JsonPatchDocument<AppointmentUpdateDTO> patchDTO)
+        public async Task<IActionResult> UpdatePartialAppointment(int id,JsonPatchDocument<AppointmentUpdateDTO> patchDTO)
         {
             if(patchDTO==null || id==0)
             {
                 return BadRequest();
             }
-            var appointment=_db.Appointments.AsNoTracking().FirstOrDefault(u=>u.Id==id);
+            var appointment=await _db.Appointments.AsNoTracking().FirstOrDefaultAsync(u=>u.Id==id);
            
             AppointmentUpdateDTO appointmentDTO = new()
             {
@@ -151,7 +151,7 @@ namespace AppointmentSchedulerAPI.Controllers
                 Reminder= appointmentDTO.Reminder,
             };
             _db.Appointments.Update(model);
-            _db.SaveChanges();
+            await _db.SaveChangesAsync();
 
             return NoContent();
         }
