90cea89ce8819692e26e3ea179178d6fb46c0792
Nirajan Karki <nirajankarki1985@gmail.com>
2023-04-11 00:29:07 +0200

    Making standard APIRespone

--- AppointmentSchedulerAPI/Controllers/AppointmentController.cs
+++ AppointmentSchedulerAPI/Controllers/AppointmentController.cs
@@ -6,6 +6,7 @@ using AutoMapper;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Net;
 
 namespace AppointmentSchedulerAPI.Controllers
 {
@@ -15,26 +16,42 @@ namespace AppointmentSchedulerAPI.Controllers
     {
         private readonly IAppointmentRepository _dbAppointment;
         private readonly IMapper _mapper;
+        protected APIResponse _response;
         public AppointmentController(IAppointmentRepository dbAppointment,IMapper mapper)
         {
             _dbAppointment= dbAppointment;
             _mapper = mapper;
+            this._response = new();
         }
 
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<ActionResult<IEnumerable<AppointmentDTO>>> GetAppointments()
+        public async Task<ActionResult<APIResponse>> GetAppointments()
         {
             IEnumerable<Appointment> appointmentList=await _dbAppointment.GetAllAsync();
-            return Ok(_mapper.Map<List<AppointmentDTO>>(appointmentList));
+            try
+            {
+                _response.Result = _mapper.Map<List<AppointmentDTO>>(appointmentList);
+                _response.StatusCode = HttpStatusCode.OK;
+                return Ok(_response);
+            }
+            catch(Exception ex)
+            {
+                _response.isSuccess = false;
+                _response.ErrorMessages=new List<string>() { ex.ToString()};
+            }
+            return _response;
+            
         }
 
         [HttpGet("{id:int}", Name = "GetAppointment")]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task< ActionResult<AppointmentDTO>> GetAppointment(int id)
+        public async Task< ActionResult<APIResponse>> GetAppointment(int id)
         {
+            try 
+            { 
             if (id == 0)
             {
                 return BadRequest();
@@ -45,40 +62,66 @@ namespace AppointmentSchedulerAPI.Controllers
             {
                 return NotFound();
             }
-            return Ok(_mapper.Map<AppointmentDTO>(appointment));
+            _response.Result = _mapper.Map<AppointmentDTO>(appointment);
+            _response.StatusCode=HttpStatusCode.OK;
+            return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.isSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
         }
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [ProducesResponseType(StatusCodes.Status201Created)]
-        public async Task< ActionResult<AppointmentDTO>> CreateAppointment([FromBody] AppointmentCreateDTO createDTO)
+        public async Task< ActionResult<APIResponse>> CreateAppointment([FromBody] AppointmentCreateDTO createDTO)
         {
-            if (await _dbAppointment.GetAsync(u => u.Title.ToLower() == createDTO.Title.ToLower()) != null)
+
+            try
             {
-                ModelState.AddModelError("CustomError", "Appointment already exists !");
-                return BadRequest(ModelState);
+                if (await _dbAppointment.GetAsync(u => u.Title.ToLower() == createDTO.Title.ToLower()) != null)
+                {
+                    ModelState.AddModelError("CustomError", "Appointment already exists !");
+                    return BadRequest(ModelState);
+                }
+
+
+                if (createDTO == null)
+                {
+                    return BadRequest();
+                }
+
+
+                Appointment appointment = _mapper.Map<Appointment>(createDTO);
+
+
+                await _dbAppointment.CreateAsync(appointment);
+                _response.Result = _mapper.Map<AppointmentCreateDTO>(appointment);
+                _response.StatusCode = HttpStatusCode.Created;
+                _response.isSuccess = true;
+
+                return CreatedAtRoute("GetAppointment", new { id = appointment.Id }, _response);
+
             }
-
-
-            if (createDTO == null)
+            catch (Exception ex)
             {
-                return BadRequest();
+                _response.isSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
             }
-            
+            return _response;
 
-            Appointment model=_mapper.Map<Appointment>(createDTO);
-           
-
-            await _dbAppointment.CreateAsync(model);
-
-            return CreatedAtRoute("GetAppointment", new { id = model.Id }, model);
         }
         [HttpDelete("{id:int}", Name = "DeleteAppointment")]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> DeleteAppointment(int id)
+        public async Task<ActionResult<APIResponse>> DeleteAppointment(int id)
         {
+            try
+            { 
             if (id == 0)
             {
                 return BadRequest();
@@ -90,34 +133,55 @@ namespace AppointmentSchedulerAPI.Controllers
             }
 
             await _dbAppointment.RemoveAsync(appointment);
+            _response.StatusCode= HttpStatusCode.NoContent;
+            _response.isSuccess=true;
             
-            return NoContent();
+            return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.isSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
         }
         [HttpPut("{id:int}", Name = "UpdateAppointment")]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
 
-        public async Task<IActionResult> UpdateAppointment(int id,[FromBody]AppointmentUpdateDTO updateDTO)
+        public async Task<ActionResult<APIResponse>> UpdateAppointment(int id,[FromBody]AppointmentUpdateDTO updateDTO)
         {
+            try
+            { 
             if(updateDTO==null || id!=updateDTO.Id)
             {
                 return BadRequest();
             }
 
-            Appointment model = _mapper.Map<Appointment>(updateDTO);
+            Appointment appointment = _mapper.Map<Appointment>(updateDTO);
             
-            await _dbAppointment.UpdateAsync(model);
+            await _dbAppointment.UpdateAsync(appointment);
+            _response.StatusCode= HttpStatusCode.NoContent;
+            _response.isSuccess=true;
             
-            
-            return NoContent();
+            return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.isSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
         }
         [HttpPatch("{id:int}",Name="UpdatePartialAppointment")]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
 
 
-        public async Task<IActionResult> UpdatePartialAppointment(int id,JsonPatchDocument<AppointmentUpdateDTO> patchDTO)
+        public async Task<ActionResult<APIResponse>> UpdatePartialAppointment(int id,JsonPatchDocument<AppointmentUpdateDTO> patchDTO)
         {
+            try
+            { 
             if(patchDTO==null || id==0)
             {
                 return BadRequest();
@@ -132,9 +196,18 @@ namespace AppointmentSchedulerAPI.Controllers
             Appointment model = _mapper.Map<Appointment>(appointmentDTO);
             
             await _dbAppointment.UpdateAsync(model);
+            _response.StatusCode= HttpStatusCode.NoContent;
+            _response.isSuccess=true;
             
 
-            return NoContent();
+            return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.isSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
         }
     }
 }
---
+++ AppointmentSchedulerAPI/Models/APIResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace AppointmentSchedulerAPI.Models
+{
+    public class APIResponse
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public bool isSuccess { get; set; } = true;
+        public List<string> ErrorMessages { get; set; }
+        public object Result { get; set; }
+
+    }
+}
